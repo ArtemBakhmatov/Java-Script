19129117163642d832e614f6a1ac6fb3dba3e3cf
Artem <ip.bakhmatov@mail.ru>
2021-05-10 13:29:21 +0300

    Практика. Пишем приложение. Часть 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", ""),
+let money = +prompt("Ваш бюджет на месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
@@ -10,12 +10,76 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    a2 = prompt("Во сколько обойдется?", ""),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    a4 = prompt("Во сколько обойдется?", "");
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt ("Во сколько обойдется?", "");
 
-    appData.expenses.a1 = a2;
-    appData.expenses.a3 = a4;
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
 
-    alert(appData.budget / 30);
+        console.log ("done");
+
+        appData.expenses[a] = b;
+    } else {                            
+        console.log ("bad result");
+        i--;
+    }
+
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
+}
+
+
+
+// if (num < 49) {
+//     console.log("Неверно!")
+// } else if (num > 100) {
+//     console.log("Много!")
+// } else {
+//     console.log("Верно!")
+// }
+
+// switch (num) {
+//     case num < 49:
+//         console.log("Неверно!");
+//         break;
+//     case num > 100:
+//         console.log("Много!");
+//         break;
+//     case num > 80:
+//         console.log("всё ещё много!");
+//         break;
+//     case num  50:
+//         console.log("Верно!");
+//         break;
+//     default:
+//         console.log("Что-то пошло не так");
+//         break;
+// };
+
+// let num = 50;
+// // while (num < 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55)
+
+// for ()
